bad70e23480c69ba6f8ef5af27ac798c5b45886e
srikanth152113 <srikanth152113@gmail.com>
2026-01-10 20:19:59 +0530

    updated

--- tools-setup/variables.tf
+++ tools-setup/variables.tf
@@ -1,7 +1,9 @@
 variable "tools" {
+    default = {
     vault = {
-      vm_size = "Standard_B2als _v2"
+    vm_size = "Standard_B2als _v2"
  }
+}
 }
 variable "rg_name" {}
 variable "rg_location" {}
