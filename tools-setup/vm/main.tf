10971c41c70b52c2e38662801649b3bd64776a34
srikanth152113 <srikanth152113@gmail.com>
2026-01-12 06:13:41 +0530

    updated

--- tools-setup/vm/main.tf
+++ tools-setup/vm/main.tf
@@ -2,8 +2,10 @@ resource "azurerm_public_ip" "publicip" {
   name                = var.name
   location            = var.rg_location
   resource_group_name = var.rg_name
-  allocation_method   = "Static"
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
 }
+
 
 resource "azurerm_network_interface" "privateip" {
   name                = var.name
@@ -14,42 +16,65 @@ resource "azurerm_network_interface" "privateip" {
     name                          = var.name
     subnet_id                     = "/subscriptions/f7fcf972-1a3d-456c-a347-914d12f8c308/resourceGroups/rg-roboshop/providers/Microsoft.Network/virtualNetworks/roboshop-network/subnets/default"
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.publicip.id
+    public_ip_address_id          = azurerm_public_ip.publicip.id
   }
 }
+
 
 resource "azurerm_network_interface_security_group_association" "nsg-attach" {
   network_interface_id      = azurerm_network_interface.privateip.id
   network_security_group_id = "/subscriptions/f7fcf972-1a3d-456c-a347-914d12f8c308/resourceGroups/rg-roboshop/providers/Microsoft.Network/networkSecurityGroups/roboshop-nsg"
 }
 
-resource "azurerm_virtual_machine" "vm" {
-  name                  = var.name
-  location              = var.rg_location
-  resource_group_name   = var.rg_name
-  network_interface_ids = [azurerm_network_interface.privateip.id]
-  vm_size               = var.vm_size
-  delete_os_disk_on_termination = true
+############# We moved to spot instance for saving the cost
+# resource "azurerm_virtual_machine" "vm" {
+#   name                  = var.name
+#   location              = var.rg_location
+#   resource_group_name   = var.rg_name
+#   network_interface_ids = [azurerm_network_interface.privateip.id]
+#   vm_size               = var.vm_size
+#   delete_os_disk_on_termination = true
 
-  storage_image_reference {
-   id = "/subscriptions/f7fcf972-1a3d-456c-a347-914d12f8c308/resourceGroups/rg-roboshop/providers/Microsoft.Compute/images/local-devops-practice"
+#   storage_image_reference {
+#    id = "/subscriptions/f7fcf972-1a3d-456c-a347-914d12f8c308/resourceGroups/rg-roboshop/providers/Microsoft.Compute/images/local-devops-practice"
+#   }
+#   storage_os_disk {
+#     name              = "${var.name}-disk"
+#     caching           = "ReadWrite"
+#     create_option     = "FromImage"
+#     managed_disk_type = "Standard_LRS"
+#   }
+#   os_profile {
+#     computer_name  = var.name
+#     admin_username = "azureuser"
+#     admin_password = "DevOps@123456"
+#   }
+#  os_profile_linux_config {
+#     disable_password_authentication = false
+#   }
+# }
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                            = var.name
+  location                        = var.rg_location
+  resource_group_name             = var.rg_name
+  size                            = var.vm_size
+  admin_username                  = "azureuser"
+  admin_password                  = "DevOps@123456"
+  disable_password_authentication = false
+  network_interface_ids           = [azurerm_network_interface.privateip.id]
+
+  os_disk {
+    name                 = "${var.name}-disk"
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
   }
-  storage_os_disk {
-    name              = "${var.name}-disk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-  os_profile {
-    computer_name  = var.name
-    admin_username = "azureuser"
-    admin_password = "DevOps@123456"
-  }
- os_profile_linux_config {
-    disable_password_authentication = false
-  }
+  source_image_id = "/subscriptions/f7fcf972-1a3d-456c-a347-914d12f8c308/resourceGroups/rg-roboshop/providers/Microsoft.Compute/images/local-devops-practice"
+  
+  # Spot Details
+  priority        = "Spot"
+  max_bid_price   = -1
+  eviction_policy = "Deallocate"
 }
-
 resource "azurerm_dns_a_record" "public_dns_record" {
   name                = var.name
   zone_name           = "srikanth553.store"
