8f5eafd44de3f159cc16a31b74cfaf5252903f09
srikanth152113 <srikanth152113@gmail.com>
2026-01-12 06:16:37 +0530

    updated

--- tools-setup/vm/main.tf
+++ tools-setup/vm/main.tf
@@ -2,7 +2,7 @@ resource "azurerm_public_ip" "publicip" {
   name                = var.name
   location            = var.rg_location
   resource_group_name = var.rg_name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Standard"
 }
 
